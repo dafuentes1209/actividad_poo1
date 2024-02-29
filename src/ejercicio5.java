5e60e09bd8c295fdbc7881496046b12fa6c17188
david <dafuentesc@unal.edu.co>
2024-02-28 22:21:35 -0500

    tercer commit

--- src/ejercicio5.java
+++ src/ejercicio5.java
@@ -1,3 +1,15 @@
 public class ejercicio5 {
-    
+    public static void main(String[] args) {
+        double suma, x,y;
+        suma = 0;
+        x = 20;
+        suma = suma + x;
+        y = 40;
+        x= x+ Math.pow(y,2);
+        suma = suma +(x/y);
+        System.out.println("EL VALOR DE LA SUMA ES: "+ suma);
+    }
+
 }
+
+
