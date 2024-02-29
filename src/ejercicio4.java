af5c31163607d42bfaccce353c795c640496dacf
david <dafuentesc@unal.edu.co>
2024-02-28 21:35:47 -0500

    primer commit

---
+++ src/ejercicio4.java
@@ -0,0 +1,17 @@
+public class ejercicio4 {
+        public static void main(String[] args) {
+            int edad_juan = 27;
+            int edad_ana, edad_alberto, edad_mama;
+            edad_alberto = 2*(edad_juan/3);
+            edad_ana = 4* (edad_juan/3);
+            edad_mama = edad_juan + edad_alberto + edad_ana;
+            System.out.println("Las edades son: ");
+            System.out.println("Alberto : "+ edad_alberto);
+            System.out.println("Juan : " + edad_juan);
+            System.out.println("Ana : " + edad_ana);
+            System.out.println("Mamá : " + edad_mama);
+        }
+
+    }
+
+
