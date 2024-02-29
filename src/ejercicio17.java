9c96afd8a637941e90f354e4bea628fcbb7ac4d2
david <dafuentesc@unal.edu.co>
2024-02-28 23:03:29 -0500

    quinto commit

---
+++ src/ejercicio17.java
@@ -0,0 +1,9 @@
+public class ejercicio17 {
+    public static void main(String[] args) {
+        double radio = 7.0;
+        double area = Math.PI * Math.pow(radio, 2);
+        double longitudCircunferencia = 2 * Math.PI * radio;
+        System.out.println("El área del círculo es: " + area);
+        System.out.println("La longitud de la circunferencia es: " + longitudCircunferencia);
+    }
+}
